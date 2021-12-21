94efb82407276bad16cb786d886cb568c925ed14
Daymon Littrell <unstopbole@gmail.com>
2021-12-21 00:06:46 -0600

    Implemented functionality for lifecycle.

--- src/lifecycle.c
+++ src/lifecycle.c
@@ -1,1 +1,20 @@
 #include "../headers/lifecycle.h"
+
+_Noreturn void lifecycle(void){
+    while(true){
+        prompt("$");
+
+        char* line = read_line(stdin);
+        if(are_equal(line, "\n")){
+            cleanup(V &line);
+            continue;
+        }
+        s_command** commands = tokenizer(line);
+        s_command* command_tree = parse(commands);
+        execute(command_tree);
+
+        de_command(command_tree);
+        cleanup(V &commands);
+        cleanup(V &line);
+    }
+}
